034d9902401e107a74ed3ed3b7a2fb60b60a3a05
Neldirson Castañeda <neldirson12@hotmail.com>
2022-03-13 21:07:26 -0500

    Commit inicial

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "project_name" {
+  type        = string
+  default     = "sistecredito"
+  description = "Project name"
+}
+
+
+variable "location" {
+  type        = string
+  default     = "Central US"
+  description = "Azure project location"
+}
