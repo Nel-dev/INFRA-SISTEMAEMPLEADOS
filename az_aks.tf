c9379bd6fe9050492e2ac211bbdc7d71daa2e732
Neldirson Castañeda <neldirson12@hotmail.com>
2022-03-16 21:15:41 -0500

    correcion menor

--- az_aks.tf
+++ az_aks.tf
@@ -19,10 +19,11 @@ resource "azurerm_kubernetes_cluster" "this" {
   }
 }
 
+/*
 resource "azurerm_role_assignment" "this" {
-  principal_id                     = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
+  principal_id                     = azurerm_kubernetes_cluster.this.kubelet_identity.0.object_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
 }
-
+*/
